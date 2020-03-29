ce317c2904de0703b9ee11f1b82b441483f73ea6
JonthueM <connect@jonthuemichel.com>
2020-03-29 09:47:46 -0400

    Update File Structure on JS and JS Basic 10

---
+++ JavaScript_Projects/Basic_JavaScript_10.js
@@ -0,0 +1,67 @@
+alert("Everything id connected!");
+//Counting Strings
+function stringCount() {
+    var string = "Hello World!";
+    var count = string.length;
+    document.getElementById("countString").innerHTML = count;
+
+}
+//For Loop
+var Instruments = ["Guitar", "Drums", "Piano", "Bass","Violin","Trumpt","Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>;"
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+//Array
+function kinds_of_fruits(){
+    var different_fruits = [];
+    different_fruits[0] = "Banana";
+    different_fruits[1] = "Kiwi";
+    different_fruits[2] = "Apple";
+    different_fruits[3] = "Mango";
+    document.getElementById("Array").innerHTML = "I love " + different_fruits[0] + "s."
+}
+// Constant
+function constant_function() {
+    const kind_of_vegetable = {type:"greens", name:"Collards", size:"small"};
+    kind_of_vegetable.size = "medium";
+    kind_of_vegetable.price = "$3.50";
+    document.getElementById("constant").innerHTML = kind_of_vegetable.type + " taste good!";
+}
+
+var X = 907;
+document.getElementById("before_LET").innerHTML = X;
+{
+    let X = 777;
+    document.getElementById("LET").innerHTML = X;
+}
+document.getElementById("after_LET").innerHTML = X;
+
+function returnThis(){
+    return Date();
+    }
+document.getElementById("stopAndGo").innerHTML = returnThis();
+
+let phone = {
+    make: "S10 ",
+    model:"Galaxy ",
+    year: "2018 ",
+    color:"black ",
+    description: function () {
+        return "I have a " + this.color + this.year + this.model + this.make;
+    }
+
+};
+document.getElementById("LET_Object").innerHTML = phone.description();
+
+var text = "";
+var i;
+for (i = 0; i < 10; i++) {
+    if (i === 7) { break; }
+    text += "Number " + i + "<br>";
+}
+document.getElementById("break").innerHTML = text;
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # The-Tech-Academy
-Assigments &amp; Projects
+[Assignment](#todo)
+
+NOTE: This is steadliy being updated and Improved. Most of the files are named according to the lesson number to make it easier for teachers to 4 Below is an TOC of assignments & challenges. 
+
+## Assignment &amp; Projects
+
+
+HTML & CSS | Description | JavaScript | Description
+-----------|-------------|------------|-------------
+1 Test | 2 Test  | 3 Test | 4 Test
+
+
+## Todo
+
+Past Assignment to fix and other things
+
+### Past Assignment
+- [ ] Double Check if my current repo is public or private
+- [ ] Review and Correct HTML CSS Page 298
+- [ ] Review and Correct HTML CSS Page 319
+- [ ] Review and Correct HTML CSS Page 342
+- [ ] Review and Correct HTML CSS Page 367
+- [ ] Review and Correct HTML CSS Page 367
+- [ ] Review and Correct HTML CSS Page 419
+- [ ] Review and Correct JS Page 58
+- [ ] Review and Correct JS Page 63
+- [ ] Review and Correct JS Page 115
+- [ ] Review and Correct JS Page 128
+- [ ] Review and Correct JS Page 139
+
+### Track
+- [ ] Finish JavaScript
+
+### Other Things
+- [ ] Add logo
+- [ ] Add Website URL
+- [ ] Learn more stuff about GitHub and markdown
+
+
