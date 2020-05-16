11444af1f26f287504b1211cc4b7afafafb5877e
JonthueM <connect@jonthuemichel.com>
2020-05-16 14:06:58 -0400

    Fix the for loop

--- 3_Basic_JavaScript/Basic_JavaScript_1.js
+++ 3_Basic_JavaScript/Basic_JavaScript_1.js
@@ -1,9 +1,10 @@
 
 var first = "Hello";
 var Words = " World!";
-
+// Pre assign values to these global variables
 
 var common = first + Words;
+//Bringing them together
 
 alert(common)
 
@@ -13,3 +14,4 @@ function My_First_Function() {
 	var result = common.fontcolor("black");
 document.getElementById("Black_Text").innerHTML = result;
 }
+// Assiging a variable that holds a combine string values but modifying it's color to black upon the click of a button
